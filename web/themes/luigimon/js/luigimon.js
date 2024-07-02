a707bb673a6569ada93e006f74c09baf1f3cd3d8
Ralph Spencer Lacson <124493306+ralphscl@users.noreply.github.com>
2024-07-02 05:07:26 +0000

    Created new theme

---
+++ web/themes/luigimon/js/luigimon.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Luigimon behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.luigimon = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
