fcbc5db44a99d33cdd1fd3d01b131f13f21e0272
Marjorie Aquino <68543285+aquinomrj@users.noreply.github.com>
2024-07-03 08:31:22 +0000

    Initial FE

--- web/themes/luigimon/js/luigimon.js
+++ web/themes/luigimon/js/luigimon.js
@@ -7,11 +7,31 @@
   'use strict';
 
   Drupal.behaviors.luigimon = {
-    attach (context, settings) {
-
-      console.log('It works!');
-
+    attach(context, settings) {
+      once('luigimon-page', 'body', context).forEach(
+        function (element) {
+          console.log('It works!');
+          var $temp = $("<input>");
+          var $url = $(location).attr("href");
+          $(".btn-share").click(function () {
+            try {
+              $("body").append($temp);
+              $temp.val($url).select();
+              document.execCommand("copy");
+              $temp.remove();
+              $('[data-toggle="tooltip"]').tooltip("show");
+              // copied animation
+              setTimeout(function () {
+                $('[data-toggle="tooltip"]').tooltip("dispose");
+              }, 1500);
+            } catch (err) {
+              //fallback in case exexCommand doesnt work
+              // alert("please press Ctrl/Cmd+C to copy");
+            }
+          });
+        }
+      );
     }
   };
 
-} (Drupal));
+}(Drupal));
